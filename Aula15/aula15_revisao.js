a313f70edf10aaa08cc8e0aa099ca6e24a340e61
Gabriel Pereira <gpereira@blueedtech.com.br>
2022-05-02 22:03:05 -0300

    aula 16 - Promises

--- Aula15/aula15_revisao.js
+++ Aula15/aula15_revisao.js
@@ -81,79 +81,79 @@ ela deve crescer 5 cm.
 Crie um método bio que retorna uma string com todas as informações da pessoa.
 */
 
-let nome = prompt('Qual o nome? ');
-let idade = +prompt('Qual a idade? ');
-let peso = +prompt('Qual o peso (kg)? ');
-let altura = +prompt('Qual a altura (m)? ');
+// let nome = prompt('Qual o nome? ');
+// let idade = +prompt('Qual a idade? ');
+// let peso = +prompt('Qual o peso (kg)? ');
+// let altura = +prompt('Qual a altura (m)? ');
 
-let pessoa = {
-    nome: nome,
-    idade: idade,
-    peso: peso,
-    altura: altura,
+// let pessoa = {
+//     nome,
+//     idade,
+//     peso,
+//     altura,
 
-    envelhecer: function(){
-        if (this.idade < 21){
-            this.idade++
-            this.altura += 0.05
-            this.altura = this.altura.toFixed(2)
-        } else {
-            this.idade++
-        }
-    },
+//     envelhecer: function(){
+//         if (this.idade < 21){
+//             this.idade++
+//             this.altura += 0.05
+//             this.altura = this.altura.toFixed(2)
+//         } else {
+//             this.idade++
+//         }
+//     },
 
-    // Método de envelhecer mais de 1 ano
-    envelhecerAnos: function(anos = 1){
-        for (let i = 0; i < anos; i++){
-            if (this.idade < 21){
-                this.idade++
-                this.altura += 0.05
-                this.altura.toFixed(2)
-                Number(this.altura);
-            } else {
-                this.idade++
-            }
-        }
-    },
+//     // Método de envelhecer mais de 1 ano
+//     envelhecerAnos: function(anos = 1){
+//         for (let i = 0; i < anos; i++){
+//             if (this.idade < 21){
+//                 this.idade++
+//                 this.altura += 0.05
+//                 this.altura.toFixed(2)
+//                 Number(this.altura);
+//             } else {
+//                 this.idade++
+//             }
+//         }
+//     },
 
-    engordar: function(peso = 1){
-        // this.peso = this.peso + peso;
+//     engordar: function(peso = 1){
+//         // this.peso = this.peso + peso;
         
-        // Outra forma, mesmo resultado:
-        this.peso += peso
-    },
+//         // Outra forma, mesmo resultado:
+//         this.peso += peso
+//     },
 
-    emagrecer: function(peso = 1){
-        this.peso -= peso
-    },
+//     emagrecer: function(peso = 1){
+//         this.peso -= peso
+//     },
 
-    crescer: function(){
-        this.altura++
-    },
+//     crescer: function(){
+//         this.altura++
+//     },
 
-    bio: function(){
-        return `
-        Dados de ${this.nome}:
-        Altura: ${this.altura}
-        Peso: ${this.peso}
-        Idade: ${this.idade}
-        `
-    }
-};
+//     bio: function(){
+//         return `
+//         Dados de ${this.nome}:
+//         Altura: ${this.altura}
+//         Peso: ${this.peso}
+//         Idade: ${this.idade}
+//         `
+//     }
+// };
 
-console.log(pessoa.bio());
-console.log();
+// console.log(pessoa.bio());
+// console.log();
 
 
 
 // let quilos = +prompt('Quantos quilos a pessoa vai engordar? ')
 // pessoa.engordar(quilos);
 
-let anos = +prompt('Quantos anos a pessoa vai envelhecer? ')
-pessoa.envelhecerAnos(anos);
-console.log();
+// let anos = +prompt('Quantos anos a pessoa vai envelhecer? ')
+// pessoa.envelhecerAnos(anos);
+// console.log();
 
-console.log(pessoa.bio());
+// console.log(pessoa.bio());
 
 
 
@@ -237,5 +237,15 @@ console.log(pessoa.bio());
 // while (prompt() != ''){
 //     console.log('Digite apenas ENTER');
 // }
+
+
+
+
+
+
+
+
+
+
 
 console.log();
---
+++ Aula16/aula16_promises.js
@@ -0,0 +1,223 @@
+console.clear();
+const prompt = require('prompt-sync')();
+
+// function pedirPizza(){
+//     console.log('Chego na Pizzaria');
+//     console.log('Escolho o sabor');
+//     console.log('Peço a pizza');
+//     console.log('Espero a pizza ficar pronta');
+//     console.log('Vou embora com a pizza');
+// }
+
+// pedirPizza();
+
+// console.log('Comer a pizza.');
+
+
+
+
+
+
+
+// // Essa função será chamada como callback, ou seja, vai ser chamada por outra função.
+// // Ela tem parâmetros declarados normalmente, e esses argumentos serão passados também dentro da outra função.
+// // Assim como acontece em qualquer função, o nome do parâmetro declarado, e o argumento passado, não precisam ser os mesmos.
+// function pizzaPronta(pizza, valor) {
+//     console.log(`Sua pizza de ${pizza} está pronta.`);
+//     console.log(`Ficou em R$${valor}`)
+// }
+
+// function acabouSabor(pizza) {
+//     console.log(`Nós não temos mais pizza de ${pizza}.`);
+// }
+  
+// function pedirPizza(callback) {
+//     console.log('Cheguei na pizzaria.')
+//     const sabor = prompt('Escolha o sabor da sua pizza: ');
+//     const preco = 50.00
+
+//     // Nesse momento, ela vai chamar a função que foi passada como argumento.
+//     // A vantagem disso, é que ao invés dessa função (pedirPizza) sempre chamar a mesma,
+//     // Eu posso usá-la para chamar funções diferentes dependendo da situação, o que torna minha função mais versátil.
+//     // Ela pode receber argumentos normalmente.
+//     callback(sabor, preco);
+// }
+
+
+// let calabresa = prompt('Tem pizza de calabresa? ');
+// if (calabresa == 'sim'){
+//     // Caso sim, chamo a função inicial e passo como argumento uma função que faz a pizza ficar pronta.
+//     pedirPizza(pizzaPronta);
+// } else {
+//     // Caso não, chamo a função inicial e passo como argumento uma função que diz que acabou a pizza.
+//     pedirPizza(acabouSabor);
+// }
+
+// // As funções chamadas nesse caso ainda são síncronas.
+// // Então apenas após a conclusão das duas é que o programa vai continuar.
+// console.log('Vou para casa.');
+
+
+
+
+
+
+
+
+// //          ### FUNÇÕES ASSÍNCRONAS
+// //          # Promises
+
+// function primeiraFuncao(parametro) {
+//     return new Promise((resolve, reject) => {
+//         console.log(`Essa é a primeira função, ela escreve: ${parametro}`);
+ 
+//         resolve(parametro);
+//     });
+// }
+
+// function segundaFuncao(parametro) {
+//     return new Promise((resolve, reject) => {
+//         const novoParametro = parametro + 10;
+//         console.log(`Essa vai adicionar 10, totalizando: ${novoParametro}`);
+ 
+//         resolve(novoParametro);
+//     });
+// }
+
+// function terceiraFuncao(parametro) {
+//     return new Promise((resolve, reject) => {
+//         const novoParametro = parametro * 2;
+//         console.log(`Essa é a terceira função, ela dobra o valor: ${novoParametro}`);      
+
+//         resolve(novoParametro);
+//     });
+// }
+
+// function quartaFuncao(parametro) {
+//     return new Promise((resolve, reject) => {
+//         const novoParametro = parametro - 15;
+//         console.log(`Essa é a quarta função, subtraindo 15: ${novoParametro}`);
+    
+//         resolve(novoParametro);
+//     });
+// }
+
+// let numero = +prompt('Escolha um número a ser zoado: ')
+
+// primeiraFuncao(numero)
+//     .then(segundaFuncao)
+//     .then(terceiraFuncao)
+//     .then(quartaFuncao);
+
+
+
+
+
+
+
+
+
+// //         # Tratando erros com o catch
+
+// function alteraNome(parametro) {
+//     return new Promise((resolve, reject) => {
+//         parametro = parametro.toUpperCase();
+
+//         resolve(parametro);
+//     });
+// }
+
+// function escreveNome(parametro) {
+//     return new Promise((resolve, reject) => {
+//         console.log(`Aqui está o nome em MAIÚSCULAS: ${parametro}`);
+
+//         resolve(parametro);
+//     });
+// }
+
+// // let nome = prompt('Escreva um nome para passar para maiúsculas: ');
+
+// alteraNome(13)
+//     .then(escreveNome)
+//     .catch((err) => console.log('Alguma coisa deu errada no caminho...', err));
+
+
+
+
+
+
+
+
+
+
+let personagem = {
+    cansaco: 0,
+    vital: 10,
+    peso: 80,
+
+    horas: 6,
+    dia: 1,
+
+    comer: function(){
+        this.peso++
+    },
+
+    bio: function(){
+        if (this.horas >= 19){
+            console.log(`
+            Está de noite.
+            `)
+        } else if(this.horas >= 12){
+            console.log(`
+            Está de tarde.
+            `)
+        } else if(this.horas >= 6){
+            console.log(`
+            Está de manhã.
+            `)
+        } else {
+            console.log(`
+            Está de madrugada.`)
+        }
+        console.log(`
+        São ${this.horas} horas do dia ${this.dia}.
+        Você está:
+        Vitalidade: ${this.vital}
+        Cansaço: ${this.cansaco}
+        Peso: ${this.peso} kg.
+        `)
+    },
+
+    passaTempo: function(horas){
+        this.horas += horas
+        if (this.horas >= 24){
+            this.horas -= 24;
+            this.dia++
+        }
+    }
+}
+
+
+while(true){ 
+    console.clear();
+    personagem.bio();
+
+    console.log('Escolha a sua ação:');
+    console.log('1 - Trabalhar');
+    console.log('2 - Comer');
+    console.log('3 - sair');
+    let escolha = prompt('Escolha o que fazer: ')
+
+    if (escolha == 1){
+        console.log('Ele foi trabalhar')
+        personagem.passaTempo(8)
+    } else if (escolha == 2){
+        console.log('Você comeu');
+        personagem.comer();
+        personagem.passaTempo(1)
+    } else if (escolha == 3){
+        break;
+    } 
+}
+
+console.log();
